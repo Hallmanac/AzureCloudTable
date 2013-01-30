3da360d0b8b69eac63d52af7fb2af26de201d37b
Brian Hall <Brian@Hallmanac.com>
2013-01-30 18:33:33 -0500

    Deleted the IPartitionScheme interface. I went with using a list of Tuples instead (inside the CloudTableContex class).

--- HallmanacAzureTableEventStore/IPartitionScheme.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HallmanacAzureTable.EventStore
-{
-    public interface IPartitionScheme<TDomainEntity> where TDomainEntity : class, new()
-    {
-        string PartitionKeyName { get; }
-
-        bool MeetsPartitionRequirements(TDomainEntity domainEntity, out AzureTableEntity<TDomainEntity> tableEntity );
-    }
-}
