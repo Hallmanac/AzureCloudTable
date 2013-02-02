4025589ec66cbc3ee489ebaee845e4ebca082092
Brian Hall <Brian@Hallmanac.com>
2013-02-01 22:39:31 -0500

    Added untracked files from the reanaming fiasco.

---
+++ AzureCloudTable.Api/TableMetaData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HallmanacAzureTable.EventStore
+{
+    public class TableMetaData<TDomainObject> where TDomainObject : class, new()
+    {
+        public Dictionary<string, Func<TDomainObject, bool>> PartitionSchemes { get; set; } 
+
+        public TableMetaData()
+        {
+            PartitionSchemes = new Dictionary<string, Func<TDomainObject, bool>>();
+        }
+    }
+}
