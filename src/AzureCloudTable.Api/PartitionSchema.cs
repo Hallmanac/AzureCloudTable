9402c1b4e3271140ff8f475788e3962b8a5af771
Brian Hall <Brian@Hallmanac.com>
2013-04-29 08:22:57 -0400

    Simplified the class so that there's only one partition key per schema instance. Made the initialization a fluent API and set defaults on the properties (via backing fields).

--- src/AzureCloudTable.Api/PartitionSchema.cs
+++ src/AzureCloudTable.Api/PartitionSchema.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ServiceStack.Text;
 
 namespace AzureCloudTableContext.Api
 {
@@ -9,92 +10,90 @@ namespace AzureCloudTableContext.Api
     /// <typeparam name="TDomainObject">The POCO class that is being written to Azure Table Storage</typeparam>
     public class PartitionSchema<TDomainObject> where TDomainObject : class, new()
     {
-        /// <summary>
-        ///     Creates a new PartitionScheme object.
-        /// </summary>
-        /// <param name="schemaName">Value that will be used as the PartitionKey in the Azure TableEntity</param>
-        /// <param name="validateEntityForPartition">
-        ///     Lambda expression that determines if it meets the requirements of the
-        ///     partition schema. If all entities need to be in this partition simply return true. Example:
-        ///     <para>entity => entity.SomeProperty == SomeValue</para>
-        ///     <para>entity => true</para>
-        /// </param>
-        /// <param name="setPartitionKey">
-        ///     Lambda expression that sets the PartitionKey for the table entity. Gives ability to map the value of an
-        ///     entity property to the PartitionKey. Make sure your property will conform to PartitionKey constraints 
-        ///     (i.e. no bigger than 1K, etc). The default value is that of the SchemaName. Example:
-        ///     <para>entity => entity.SomeProperty.ToString()</para>
-        ///     <para>entity => entity.SomeProperty.ToJsv() -- ServiceStack.Text serializer --</para>
-        /// </param>
-        /// <param name="setIndexedPropValue">
-        ///     Lambda expression that sets the value of the Indexed Property on the Table Entity. The default
-        ///     returns an empty string. Example:
-        ///     <para>
-        ///         <example>entity => entity.SomeProperty</example>
-        ///     </para>
-        /// </param>
-        /// <param name="setRowKey">
-        ///     Lambda expression that sets the RowKey value. Gives the ability to map the value of an entity property
-        ///     to the RowKey. Make sure your property will conform to RowKey constraints (i.e. no bigger than 1K, etc) and is unique.
-        ///     <para>The default value for the RowKey will be the value of the entity's Id property.</para>
-        /// </param>
-        public PartitionSchema(string schemaName = "DefaultSchemaName", Func<TDomainObject, bool> validateEntityForPartition = null, Func<TDomainObject, string> setPartitionKey = null, Func<TDomainObject, object> setIndexedPropValue = null, Func<TDomainObject, string> setRowKey = null)
-        {
-            SchemaName = schemaName;
-            Init(validateEntityForPartition, setPartitionKey, setIndexedPropValue, setRowKey);
-        }
+        private Func<TDomainObject, bool> _partitionCriteriaMethod;
+        private Func<TDomainObject, string> _setRowKey;
+        private Func<TDomainObject, object> _getIndexedPropertyFromCriteria;
 
         /// <summary>
-        /// Unique name of PartitionSchema.
+        /// Partition Key.
         /// </summary>
-        public string SchemaName { get; set; }
+        public string PartitionKey { get; private set; }
         
         /// <summary>
-        /// Called to set the PartitionKey of the CloudTableEntity prior to saving to the table.
-        /// </summary>
-        public Func<TDomainObject, string> SetPartitionKey { get; set; }
-
-        /// <summary>
         /// Called to verify whether or not the given domain entity meets the requirements to be in the current PartitionSchema.
+        /// Default is to return true.
         /// </summary>
-        public Func<TDomainObject, bool> ValidateEntityForPartition { get; set; }
+        public Func<TDomainObject, bool> CheckAgainstPartitionCriteria
+        {
+            get
+            {
+                return _partitionCriteriaMethod ?? (_partitionCriteriaMethod = givenEntity => true);
+            }
+        }
 
         /// <summary>
         /// Called to set the RowKey of the CloudTableEntity prior to saving to the table.
         /// </summary>
-        public Func<TDomainObject, string> SetRowKey { get; set; }
+        public Func<TDomainObject, string> GetRowKeyFromCriteria
+        {
+            get
+            {
+                return _setRowKey ?? (_setRowKey = givenObj => SetChronologicalBasedRowKey());
+            }
+        }
 
         /// <summary>
         /// Called to set the IndexedProperty of the CloudTableEntity prior to saving to the table.
         /// </summary>
-        public Func<TDomainObject, object> SetIndexedProperty { get; set; }
+        public Func<TDomainObject, object> GetIndexedPropertyFromCriteria
+        {
+            get
+            {
+                return _getIndexedPropertyFromCriteria ?? (givenObj => "");
+            }
+        }
 
         /// <summary>
-        /// Holds a list of CloudTableEntity objects that belong to a certain PartitionKey. This is used internally in 
-        /// the CloudTableContext class and gets dynamically built up and destroyed.
+        /// A string for a row key that provides a default ordering of oldest to newest.
         /// </summary>
-        public Dictionary<string, List<CloudTableEntity<TDomainObject>>> CloudTableEntities { get; set; }
-
-        private void Init(Func<TDomainObject, bool> validationMethod, Func<TDomainObject, string> setPartitionKey, Func<TDomainObject, object> setIndexedPropValue,
-            Func<TDomainObject, string> setRowKey)
+        /// <returns></returns>
+        public string SetChronologicalBasedRowKey()
         {
-            if(setPartitionKey != null)
-                SetPartitionKey = setPartitionKey;
-            else
-                SetPartitionKey = entity => SchemaName;
-            if(validationMethod != null)
-                ValidateEntityForPartition = validationMethod;
-            else
-                ValidateEntityForPartition = entity => false;
-            if(setIndexedPropValue != null)
-                SetIndexedProperty = setIndexedPropValue;
-            else
-                SetIndexedProperty = entity => null;
-            if(setRowKey != null)
-                SetRowKey = setRowKey;
-            else
-                SetRowKey = entity => null;
-            CloudTableEntities = new Dictionary<string, List<CloudTableEntity<TDomainObject>>>();
+            return string.Format("{0:D20}_{1}", (DateTimeOffset.Now.Ticks), Guid.NewGuid().ToJsv());
+        }
+
+        /// <summary>
+        /// A Row key that can be used for an ordering of newest to oldest.
+        /// </summary>
+        /// <returns></returns>
+        public string SetReverseChronologicalBasedRowKey()
+        {
+            return string.Format("{0:D20}_{1}", (DateTimeOffset.MaxValue.Ticks - DateTimeOffset.Now.Ticks),
+                                 Guid.NewGuid());
+        }
+
+        public PartitionSchema<TDomainObject> SetPartitionKey(string givenPartitionKey)
+        {
+            PartitionKey = givenPartitionKey;
+            return this;
+        }
+ 
+        public PartitionSchema<TDomainObject> SetSchemaPredicateCriteria(Func<TDomainObject, bool> givenCriteria )
+        {
+            _partitionCriteriaMethod = givenCriteria;
+            return this;
+        }
+
+        public PartitionSchema<TDomainObject> SetRowKeyCriteria(Func<TDomainObject, string> givenRowKeyCriteria )
+        {
+            _setRowKey = givenRowKeyCriteria;
+            return this;
+        }
+
+        public PartitionSchema<TDomainObject>  SetIndexedPropertyCriteria(Func<TDomainObject, object> givenIndexedPropCriteria )
+        {
+            _getIndexedPropertyFromCriteria = givenIndexedPropCriteria;
+            return this;
         }
     }
 }
