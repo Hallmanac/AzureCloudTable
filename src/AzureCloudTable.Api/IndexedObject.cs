98654ed938a2571259b3af23a72ad8b5c06c389c
Brian Hall <brian@nebbiatech.com>
2017-06-04 08:31:59 -0400

    Added additional constructor to allow for setting the indexe value property from the constructor

--- src/AzureCloudTable.Api/IndexedObject.cs
+++ src/AzureCloudTable.Api/IndexedObject.cs
@@ -2,6 +2,11 @@
 {
     /// <summary>
     /// Wraps the value being indexed in a CloudTableEntity.
+    /// 
+    /// <para>
+    /// In order for the <see cref="CloudTableEntity{TDomainObject}"/> to always be able to properly serialize the 
+    /// value of a property that is to be indexed, we have to wrap it inside a class. That's what this class is for.
+    /// </para>
     /// </summary>
     public class IndexedObject 
     {
@@ -11,6 +16,17 @@
         public IndexedObject()
         {
             ValueBeingIndexed = null;
+        }
+
+
+        /// <summary>
+        /// Secondary constructor to allow for the <see cref="ValueBeingIndexed"/> property to be set 
+        /// from the constructor
+        /// </summary>
+        /// <param name="valueBeingIndexed">The value being indexed</param>
+        public IndexedObject(object valueBeingIndexed)
+        {
+            ValueBeingIndexed = valueBeingIndexed;
         }
         
         /// <summary>
