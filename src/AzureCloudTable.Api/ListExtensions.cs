94086da3a9e691e987d21ffc18954b63d64cc8e8
Brian Hall <brian@nebbiatech.com>
2017-09-03 17:12:30 -0400

    Added a List<T> extension method for creating a batch of lists. #13

---
+++ src/AzureCloudTable.Api/ListExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public static class ListExtensions
+    {
+        /// <summary>
+        ///   Takes the current list and returns a List of Lists. A.K.A. a batch of lists where each list is no larger than the
+        ///   given <see cref="batchSize" />.
+        /// </summary>
+        /// <typeparam name="T">Generic type in the List</typeparam>
+        /// <param name="currentList">The current list that this method operates on.</param>
+        /// <param name="batchSize">The max number of items to be in each list.</param>
+        /// <returns></returns>
+        public static List<List<T>> ToBatch<T>(this List<T> currentList, int batchSize)
+        {
+            var batchList = new List<List<T>>();
+            var maxBatchCount = currentList.Count < batchSize ? currentList.Count : batchSize;
+            var currentCount = 0;
+            while (currentCount < currentList.Count)
+            {
+                var batch = new List<T>();
+                batch.AddRange(currentList.Skip(currentCount).Take(maxBatchCount).ToList());
+                batchList.Add(batch);
+                currentCount += maxBatchCount;
+            }
+            return batchList;
+        }
+    }
+}
